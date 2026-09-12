0731b227ad7bb4a56bef2086feeadd7a68cdf5b8
huhengbo <huhbo1992@126.com>
2026-09-12 11:42:32 +0800

    fix: remove deprecated dns client singleflight option

--- upstream.go
+++ upstream.go
@@ -26,7 +26,7 @@ func forwardToUpstreamParallel(rt *runtimeConfig, r *dns.Msg) *dns.Msg {
 		wg.Add(1)
 		go func(ep upstreamEndpoint) {
 			defer wg.Done()
-			client := &dns.Client{SingleInflight: true, Timeout: rt.upstreamTimeout, Net: ep.Network}
+			client := &dns.Client{Timeout: rt.upstreamTimeout, Net: ep.Network}
 			if ep.Network == "tcp-tls" {
 				tlsConfig := &tls.Config{MinVersion: tls.VersionTLS12}
 				serverName := ep.ServerName
